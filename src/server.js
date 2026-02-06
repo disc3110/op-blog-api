c610aaac0576ad9291080bd16210fa956494370c
Diego Ivan Solis Cuevas <disc3110@gmail.com>
2026-02-06 14:47:29 -0500

    debug dt connection

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+console.log('DATABASE_URL in prod:', process.env.DATABASE_URL);
+
 app.listen(PORT, () => {
   console.log(`✅ Server listening on http://localhost:${PORT}`);
 });
